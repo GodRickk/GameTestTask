d7fac2b94a86778b75006e22ef2692174f30d2b1
Ilya Antipin <morlok01@inbox.ru>
2023-10-09 17:26:54 +0300

    feat: add more UI outputs

--- src/main/java/game/test/task/services/Fight.java
+++ src/main/java/game/test/task/services/Fight.java
@@ -14,7 +14,13 @@ public class Fight {
 
     public Creature doFight (Player player, Monster monster) {
         Creature winner = null;
+        int playerTurnCount = 1;
+        int monsterTurnCount = 1;
+        System.out.println("==================================Fight with monster==================================");
         while (!player.isDead() && !monster.isDead()) {
+            System.out.print("Player's turn: ");
+            System.out.print(playerTurnCount);
+            playerTurnCount++;
 
             player.attackAlgorithm(monster);
             if (player.getCurentHealth() <= (int) round((player.getMaxHealth() * 0.6))) {
@@ -22,12 +28,18 @@ public class Fight {
             }
             if (monster.isDead()) {
                 winner = player;
+                monster = null;
                 break;
             }
+
+            System.out.print("Monster's turn: ");
+            System.out.print(monsterTurnCount);
+            monsterTurnCount++;
 
             monster.attackAlgorithm(player);
             if (player.isDead()) {
                 winner = monster;
+                player = null;
                 break;
             }
         }

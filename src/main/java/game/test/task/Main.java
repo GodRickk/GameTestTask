6ebda8263436e2a0354576c840b0493f1cd191a9
Ilya Antipin <morlok01@inbox.ru>
2023-10-09 17:47:41 +0300

    test: delete tests for Creature methods

--- src/main/java/game/test/task/Main.java
+++ src/main/java/game/test/task/Main.java
@@ -11,31 +11,6 @@ public class Main {
         playerDamage[0] = 11;
         playerDamage[1] = 8;
         Player iAm = new Player(3, 4, 32, playerDamage);
-        System.out.print("Damage array: ");
-
-        // [0, 0] вывод массива наносимого урона
-        System.out.println(Arrays.toString(iAm.getDamage()));
-        //System.out.print(iAm.getCurentHealth());
-
-
-        // тест метода расчёта модификатора атаки
-        //int modifier = iAm.attackModCalc(2);
-        //System.out.println(modifier);
-
-
-//        // тест успешности броска
-//        boolean isSuccessful = iAm.isAttackSuccessful(2);
-//        System.out.println(isSuccessful);
-//
-//
-//        //тест нанесения урона
-//        System.out.println(iAm.hit(isSuccessful));
-//
-//
-//        //тест получения урона
-//        iAm.getHit(7);
-//        System.out.println(iAm.getCurentHealth());
-
 
         //тест алгоритма атаки
         int[] monsterDamage = new int[2];
